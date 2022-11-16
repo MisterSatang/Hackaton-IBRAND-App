bc420694ac83af4c036a9bcaaf27d85626d30c3c
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-11-16 16:04:04 +0700

    link navbar user

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -21,29 +22,32 @@ export default function Navbar() {
           </button>
           <div class="collapse navbar-collapse " id="navbarNavDropdown">
             <ul class="navbar-nav ms-auto bg-dark-20 rounded-pill ">
-              <button class="nav-link active bg-dark-20 rounded-pill p-2 px-5 text-light border-0 ">
+              <Link
+                to="/"
+                class="nav-link active bg-dark-20 rounded-pill p-2 px-5 text-light border-0"
+              >
                 <div class="nav-menu">
                   <i class="bi bi-house-door me-1 text-wheat"></i>Home
                 </div>
-              </button>
-              <button class="nav-link active bg-transparent p-2 mx-4 text-light border-0">
+              </Link>
+              <Link
+                to="/mylist"
+                class="nav-link active bg-transparent p-2 mx-4 text-light border-0"
+              >
                 <div>
                   {/* <i class="bi bi-heart me-1"></i> */}
                   Mylist
                 </div>
-              </button>
-              <button class="nav-link active bg-transparent p-2 mx-4 text-light border-0">
+              </Link>
+              <Link
+                to="/order"
+                class="nav-link active bg-transparent p-2 mx-4 text-light border-0"
+              >
                 <div>
                   {/* <i class="bi bi-card-checklist  me-1"></i> */}
-                  Catalog
+                  Order
                 </div>
-              </button>
-              <button class="nav-link active bg-transparent p-2 mx-4 text-light border-0">
-                <div>
-                  {/* <i class="bi bi-chat  me-1"></i> */}
-                  About
-                </div>
-              </button>
+              </Link>
             </ul>
             <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
               <button type="button" class="btn btn-outline-light rounded-pill">
