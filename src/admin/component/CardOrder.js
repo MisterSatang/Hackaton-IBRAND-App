22de5626cea2d9c0a9f50a389f17d7d842388796
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-11-22 23:57:55 +0700

    delete class btn and bg-color cardOrder

--- src/admin/component/CardOrder.js
+++ src/admin/component/CardOrder.js
@@ -1,22 +1,28 @@
 import { Fragment } from "react";
 
 export default function CardOrder({ step, header }) {
-    return (
-        <Fragment>
-            <div className="col-xl-3 col-lg-4 col-md-5 col-12 mt-3 p-3">
-                <div className="btn btn-primary p-0 w-100 rounded-5 ">
-                    <div className="rounded-cardMenuAdmin bg-dark-20 text-center p-3 text-light fw-semibold">
-                        {header}
-                    </div>
-                    <div className="text-center p-3 text-light ">
-                        <div className="row d-flex align-items-center">
-                            <div className="col-4">total :</div>
-                            <div className={`col-4 fs-2 fw-bold ${step.length > 0 ? "text-warning" : ""}`}>{step.length > 0 ? step.length : "-"}</div>
-                            <div className="col-4">order</div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <Fragment>
+      <div className="col-xl-3 col-lg-4 col-md-5 col-12 mt-3 p-3">
+        <div className="bg-navbar p-0 w-100 rounded-5 ">
+          <div className="rounded-cardMenuAdmin bg-dark-20 text-center p-3 text-light fw-semibold">
+            {header}
+          </div>
+          <div className="text-center p-3 text-light ">
+            <div className="row d-flex align-items-center">
+              <div className="col-4">total :</div>
+              <div
+                className={`col-4 fs-2 fw-bold ${
+                  step.length > 0 ? "text-warning" : ""
+                }`}
+              >
+                {step.length > 0 ? step.length : "-"}
+              </div>
+              <div className="col-4">order</div>
             </div>
-        </Fragment>
-    );
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
